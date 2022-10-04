b4d11f68bcac2e5a352a90e8d4673133a7fb869e
Daniel Boley <danboley1@gmail.com>
2022-10-04 10:23:15 -0400

    minor snytax changes, adjusted stories and storycontainer

--- phase-2-proj/src/components/SpookyStoriesContainer.js
+++ phase-2-proj/src/components/SpookyStoriesContainer.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import SpookyStories from './SpookyStories';
+import SpookyStories from "./SpookyStories";
 
 const SpookyStoriesContainer = () => {
   const [stories, setStories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/costumes")
+    fetch("http://localhost:3000/spookystories")
       .then((res) => res.json())
       .then((data) => setStories(data));
   }, []);
@@ -16,10 +16,10 @@ const SpookyStoriesContainer = () => {
 
   return (
     <div className="spooky-stories-container">
-    <h1>Spooky Stories</h1>
-    {storyCards}
-  </div>
-  )
-}
+      <h1>Spooky Stories</h1>
+      {storyCards}
+    </div>
+  );
+};
 
 export default SpookyStoriesContainer;
