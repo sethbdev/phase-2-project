8597815d8c009995f82136fef35381785f573176
Daniel Boley <danboley1@gmail.com>
2022-10-05 10:31:12 -0400

    added countdown component, adjusted dependency array for StoryDetail component

--- phase-2-proj/src/components/Countdown.js
+++ phase-2-proj/src/components/Countdown.js
@@ -1,10 +1,56 @@
 import React, { useEffect, useState } from "react";
 
 const Countdown = () => {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    let difference = +new Date(`10/31/${year}`) - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span className="countdown-timer" key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
 
   return (
     <div>
       <h1>Halloween Countdown!</h1>
+      {timerComponents.length ? (
+        timerComponents
+      ) : (
+        <h1 className="halloween-day"> Happy Halloween!</h1>
+      )}
     </div>
   );
 };
