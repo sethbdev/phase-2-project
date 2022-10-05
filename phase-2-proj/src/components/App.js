7e4aa8901d78fb9f9bea65491de71e550d82358c
Daniel Boley <danboley1@gmail.com>
2022-10-05 09:31:03 -0400

    cleared tests from db.json, added 404 redirect link

--- phase-2-proj/src/components/App.js
+++ phase-2-proj/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar";
 import Countdown from "./Countdown";
@@ -13,10 +14,10 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch>
-        <Route path="/countdown">
+        <Route exact path="/countdown">
           <Countdown />
         </Route>
-        <Route path="/costumes">
+        <Route exact path="/costumes">
           <CostumesContainer />
         </Route>
         <Route exact path="/stories">
@@ -27,6 +28,10 @@ function App() {
         </Route>
         <Route exact path="/newstory">
           <StoriesForm />
+        </Route>
+        <Route path="*">
+          <h1>Boo! 404 not found!</h1>
+          <Link to="/countdown">Return to safety!</Link>
         </Route>
       </Switch>
     </div>
